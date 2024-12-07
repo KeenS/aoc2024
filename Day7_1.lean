0a335299992d5834a638639c22fb174616089851
κeen <3han5chou7@gmail.com>
2024-12-08 05:38:28 +0900

    day 7

---
+++ Day7_1.lean
@@ -0,0 +1,42 @@
+partial def readAll (input: IO.FS.Stream) : IO ByteArray :=
+  let rec loop (acc: ByteArray): IO ByteArray := do
+    let buf ← input.read 4096
+    let acc := acc.append buf
+    if buf.size < 4096
+    then return acc
+    else loop acc
+  loop ByteArray.empty
+
+def parse(input: String): List (Nat × List Nat) :=
+  let lines := input.dropRightWhile Char.isWhitespace |>.splitOn "\n"
+  let numbers := lines.map (·.split Char.isWhitespace)
+  numbers.map (fun line =>
+    let head := line.head!
+    let tail := line.tail!
+    (head.dropRight 1 |> String.toNat!, tail.map String.toNat!)
+  )
+
+
+def canBeCalibratedAux(result: Nat) (data: List Nat) (acc: Nat) (op: Nat -> Nat -> Nat): Bool :=
+  match data with
+  | [] => result == acc
+  | n::ns => acc <= result && (canBeCalibratedAux result ns (op acc n) (. + .) || canBeCalibratedAux result ns (op acc n) (. * .))
+
+def canBeCalibrated (input: (Nat × List Nat)): Bool :=
+  let (result, data) := input
+  canBeCalibratedAux result data 0 (. + .) || canBeCalibratedAux result data 1 (. * .)
+
+#check canBeCalibrated (190, [10, 19])
+#check canBeCalibrated (3267, [81, 40, 27])
+#check not $ canBeCalibrated (83, [17, 5])
+
+def solve (input: List (Nat × List Nat)): Nat :=
+  input.filter (canBeCalibrated) |>.map Prod.fst |>.sum
+
+def main: IO Unit := do
+  let stdin ← IO.getStdin
+  let input ← readAll stdin
+  let data := parse $ String.fromUTF8! input
+  let answer := solve data
+  let stdout ← IO.getStdout
+  stdout.putStrLn s!"{answer}"
